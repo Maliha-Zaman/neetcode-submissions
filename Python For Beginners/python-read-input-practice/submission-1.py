2bdce30e52b0e64bd527188d48fe33a74e399cfb
Maliha-Zaman <95135458+Maliha-Zaman@users.noreply.github.com>
2026-09-08 18:07:15 -0400

    Add: python-read-input-practice - submission-1

---
+++ Python For Beginners/python-read-input-practice/submission-1.py
@@ -0,0 +1,15 @@
+def add_two_numbers() -> int:
+    num_string = input()
+    num_list = num_string.split(",")
+    num_list = [int(x) for x in num_list]
+    for x in range(len(num_list)-1):
+        add = num_list[x]+num_list[x+1]
+    return add
+
+
+
+# do not modify below this line
+print(add_two_numbers())
+print(add_two_numbers())
+print(add_two_numbers())
+print(add_two_numbers())
