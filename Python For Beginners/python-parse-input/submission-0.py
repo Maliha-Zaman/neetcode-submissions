05927526555456dc440179e7f5541aaac8bb2b08
Maliha-Zaman <95135458+Maliha-Zaman@users.noreply.github.com>
2026-09-08 17:52:04 -0400

    Add: python-parse-input - submission-0

---
+++ Python For Beginners/python-parse-input/submission-0.py
@@ -0,0 +1,11 @@
+from typing import List
+
+def read_integers() -> List[int]:
+    nmbr_string = input()
+    string_list = nmbr_string.split(',')
+    return [int(x) for x in string_list]
+
+# do not modify the code below
+print(read_integers())
+print(read_integers())
+print(read_integers())
